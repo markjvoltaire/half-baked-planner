8052f6b4488a9e12cc708acf53266f26e5181499
Mark Voltaire <markjvoltaire@gmail.com>
2022-02-16 15:10:14 -0500

    wrapped provider in app.js instead of index for passing test

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
-import { PlannerProvider } from './context/PlannerContext';
 
 render(
   <React.StrictMode>
-    <PlannerProvider>
-      <App />
-    </PlannerProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
